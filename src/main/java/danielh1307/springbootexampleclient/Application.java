ce5b11df1a765cdb352584a05f7fb682a37a16e0
Daniel <hamm.daniel@gmail.com>
2019-01-30 20:56:16 +0100

    Added basic authentication to HTTP call.

--- src/main/java/danielh1307/springbootexampleclient/Application.java
+++ src/main/java/danielh1307/springbootexampleclient/Application.java
@@ -2,8 +2,13 @@ package danielh1307.springbootexampleclient;
 
 import io.swagger.client.model.AddNewFilmRequest;
 import io.swagger.client.model.Film;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 public class Application {
 
@@ -13,11 +18,22 @@ public class Application {
         System.out.println("New AddNewFilmRequest created ...");
 
         RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<AddNewFilmRequest> addNewFilmRequestWithHeaders = new HttpEntity<>(addNewFilmRequest, createHeaders("user", "appl_123"));
 
         System.out.println("Calling service ...");
-        ResponseEntity<Film> response = restTemplate.postForEntity("http://localhost:8080/api/films", addNewFilmRequest, Film.class);
+        ResponseEntity<Film> response = restTemplate.postForEntity("http://localhost:8080/api/films", addNewFilmRequestWithHeaders, Film.class);
         System.out.println("Response code is: [" + response.getStatusCode().toString() + "]");
         System.out.println("Response string is: [" + response.getBody() + "]");
         System.out.println("Title is: [" + response.getBody().getTitle() + "]");
+    }
+
+    private static HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.getEncoder().encode(
+                    auth.getBytes(Charset.forName("US-ASCII")) );
+            String authHeader = "Basic " + new String( encodedAuth );
+            set( "Authorization", authHeader );
+        }};
     }
 }
